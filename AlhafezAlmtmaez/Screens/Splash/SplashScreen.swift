af68050209a019700f946e098b31354e59afb3f3
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-11-10 20:45:15 +0200

    implement: design for SplashScreen

---
+++ AlhafezAlmtmaez/Screens/Splash/SplashScreen.swift
@@ -0,0 +1,19 @@
+//
+//  SplashScreen.swift
+//  AlhafezAlmtmaez
+//
+//  Created by Ahmed Shendy on 10/11/2023.
+//
+
+import UIKit
+
+class SplashScreen: UIViewController {
+
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
